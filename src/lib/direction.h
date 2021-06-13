eda77ceb08ec35317279497be3753d9b71da6890
chenboshuo <chenboshuo.hi@163com>
2021-06-13 08:18:08 +0800

    :bug: 数据更新的时候，禁止中断

--- src/lib/direction.h
+++ src/lib/direction.h
@@ -7,6 +7,7 @@
 #ifndef DIRECTION_H
 #define DIRECTION_H
 
+// #include "elevator.h"
 #include "led_array.h"
 
 #define ARROW_LINE_SIZE 5  /// 表示方向信息的行数是5
@@ -16,6 +17,10 @@
 
 #define SELECT_LEFT_PART 0x0F   /// 选择ARROW_SYMBOL 的左半部分
 #define SELECT_RIGHT_PART 0xF0  /// 选择ARROW_SYMBOL 的左半部分
+
+#define UP 1            /// 上行记为 1
+#define NO_DIRECTION 0  /// 静止记为 0
+#define DOWN -1         /// 下行记为 -1
 
 /// 上下箭头
 static unsigned char code ARROW_SYMBOL[] = {
@@ -38,6 +43,8 @@ void move_up(unsigned char begin_bit_loc) {
   } else {
     clean_mask = 0xF0;
   }
+
+  ALLOW_INTERRUPT = FALSE;  // 关键步骤，禁止中断
   for (i = 0; i < ARROW_LINE_SIZE; ++i) {
     base_image[i + 3] |= clean_mask;
     base_image[i + 3] &=
@@ -47,10 +54,11 @@ void move_up(unsigned char begin_bit_loc) {
         | ~clean_mask        // 去掉另外半部分
         ;
   }
+  ALLOW_INTERRUPT = TRUE;  // 恢复中断
 }
 
 /**
- * 向上移动的方向标识
+ * 向下移动的方向标识
  * @param begin_bit_loc 位置 LEFT_SYMBOL 或 RIGHT_SYMBOL
  */
 void move_dowm(unsigned char begin_bit_loc) {
@@ -61,16 +69,18 @@ void move_dowm(unsigned char begin_bit_loc) {
   } else {
     clean_mask = 0xF0;
   }
+
+  ALLOW_INTERRUPT = FALSE;  // 关键步骤，禁止中断
   for (i = 0; i < ARROW_LINE_SIZE; ++i) {
     base_image[i + 3] |= clean_mask;
     base_image[i + 3] &=
-        (((ARROW_SYMBOL[i] &
-           SELECT_RIGHT_PART)  // 选择ARROW_SYMBOL上箭头部分(低位)
-          >> 4)                // 右移到低位
-         << begin_bit_loc)     // 移动到对应位置
-        | ~clean_mask          // 去掉另外半部分
-        ;
+        ((((ARROW_SYMBOL[i] &
+            SELECT_RIGHT_PART)  // 选择ARROW_SYMBOL上箭头部分(低位)
+           >> 4)                // 右移到低位
+          << begin_bit_loc)     // 移动到对应位置
+         | ~clean_mask);        // 去掉另外半部分
   }
+  ALLOW_INTERRUPT = TRUE;  // 恢复中断
 }
 
 #endif
